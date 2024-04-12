6e674f10aa2a2fa892b44c7376abeb28ab9926ee
SafraCoder <corrheag@gmail.com>
2024-04-11 23:09:13 -0400

    Final commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,6 @@
-console.log('hi this is safra');
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
